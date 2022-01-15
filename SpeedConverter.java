f6a1238fff092c21612080ae49b2e560acfd10fd
akshaywagh3 <81297179+akshaywagh3@users.noreply.github.com>
2022-01-15 11:55:48 +0530

    Add files via upload

---
+++ SpeedConverter.java
@@ -0,0 +1,30 @@
+public class SpeedConverter
+{
+    public static void main(String[] args)
+    {
+        long miles = SpeedConverter.toMilesPerHour(18);
+        System.out.println(miles);
+        SpeedConverter.printConversion(22.5);
+
+    }
+
+    public static long toMilesPerHour(double kilometersPerHour){
+        if(kilometersPerHour < 0){
+            return -1;
+        }
+        return Math.round(kilometersPerHour/1.609);
+
+    }
+
+    public static void printConversion(double kilometersPerHour){
+        if(kilometersPerHour < 0){
+            System.out.println("invalid value");
+        }else{
+            long milesPerHour = toMilesPerHour(kilometersPerHour);
+            System.out.println(kilometersPerHour + " km/h= "+milesPerHour + " mi/h");
+        }
+    }
+    {
+
+    }
+}
